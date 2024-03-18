34e4803ec4be80c06cd8b6495c933a25a8a34a0a
Paul Tanasă <pebly@tuta.com>
2024-03-18 22:07:49 +0200

    remove: recommended and standard configs

--- index.js
+++ index.js
@@ -1,1 +1,150 @@
-module.exports = require('./dist/index.json');
+module.exports = {
+  rules: {
+    'alpha-value-notation': [
+      'percentage',
+      {
+        exceptProperties: [
+          'opacity',
+          'fill-opacity',
+          'flood-opacity',
+          'stop-opacity',
+          'stroke-opacity',
+        ],
+      },
+    ],
+    'annotation-no-unknown': true,
+    'at-rule-empty-line-before': [
+      'always',
+      {
+        except: ['blockless-after-same-name-blockless', 'first-nested'],
+        ignore: ['after-comment'],
+      },
+    ],
+    'at-rule-no-vendor-prefix': true,
+    'block-no-empty': true,
+    'color-function-notation': 'modern',
+    'color-hex-alpha': 'never',
+    'color-hex-length': 'short',
+    'color-named': 'never',
+    'color-no-invalid-hex': true,
+    'comment-empty-line-before': [
+      'always',
+      {
+        except: ['first-nested'],
+        ignore: ['stylelint-commands'],
+      },
+    ],
+    'custom-media-pattern': [
+      '^([a-z][a-z0-9]*)(-[a-z0-9]+)*$',
+      {
+        message: {},
+      },
+    ],
+    'custom-property-empty-line-before': [
+      'always',
+      {
+        except: ['after-custom-property', 'first-nested'],
+        ignore: ['after-comment', 'inside-single-line-block'],
+      },
+    ],
+    'custom-property-no-missing-var-function': true,
+    'custom-property-pattern': [
+      '^([a-z][a-z0-9]*)(-[a-z0-9]+)*$',
+      {
+        message: {},
+      },
+    ],
+    'declaration-block-no-duplicate-custom-properties': true,
+    'declaration-block-no-duplicate-properties': [
+      true,
+      {
+        ignore: ['consecutive-duplicates-with-different-syntaxes'],
+      },
+    ],
+    'declaration-block-no-redundant-longhand-properties': true,
+    'declaration-block-no-shorthand-property-overrides': true,
+    'declaration-block-single-line-max-declarations': 1,
+    'declaration-empty-line-before': [
+      'always',
+      {
+        except: ['after-declaration', 'first-nested'],
+        ignore: ['after-comment', 'inside-single-line-block'],
+      },
+    ],
+    'declaration-no-important': true,
+    'font-family-name-quotes': 'always-where-recommended',
+    'font-family-no-duplicate-names': true,
+    'font-weight-notation': 'numeric',
+    'function-calc-no-unspaced-operator': true,
+    'function-linear-gradient-no-nonstandard-direction': true,
+    'function-name-case': 'lower',
+    'function-no-unknown': true,
+    'function-url-quotes': 'always',
+    'hue-degree-notation': 'angle',
+    'import-notation': 'string',
+    'keyframe-block-no-duplicate-selectors': true,
+    'keyframe-declaration-no-important': true,
+    'keyframe-selector-notation': 'percentage-unless-within-keyword-only-block',
+    'keyframes-name-pattern': [
+      '^([a-z][a-z0-9]*)(-[a-z0-9]+)*$',
+      {
+        message: {},
+      },
+    ],
+    'length-zero-no-unit': [
+      true,
+      {
+        ignore: ['custom-properties'],
+      },
+    ],
+    'lightness-notation': 'percentage',
+    'max-nesting-depth': 4,
+    'media-feature-name-no-unknown': true,
+    'media-feature-name-no-vendor-prefix': true,
+    'media-feature-range-notation': 'context',
+    'named-grid-areas-no-invalid': true,
+    'no-descending-specificity': true,
+    'no-duplicate-at-import-rules': true,
+    'no-duplicate-selectors': true,
+    'no-empty-source': true,
+    'no-invalid-double-slash-comments': true,
+    'no-irregular-whitespace': true,
+    'no-unknown-animations': true,
+    'number-max-precision': 4,
+    'property-no-unknown': true,
+    'property-no-vendor-prefix': true,
+    'rule-empty-line-before': [
+      'always-multi-line',
+      {
+        except: ['first-nested'],
+        ignore: ['after-comment'],
+      },
+    ],
+    'selector-anb-no-unmatchable': true,
+    'selector-attribute-quotes': 'always',
+    'selector-max-universal': 0,
+    'selector-no-vendor-prefix': true,
+    'selector-not-notation': 'complex',
+    'selector-pseudo-class-no-unknown': true,
+    'selector-pseudo-element-colon-notation': 'double',
+    'selector-pseudo-element-no-unknown': true,
+    'selector-type-case': 'lower',
+    'selector-type-no-unknown': [
+      true,
+      {
+        ignore: ['custom-elements'],
+      },
+    ],
+    'shorthand-property-no-redundant-values': true,
+    'string-no-newline': true,
+    'time-min-milliseconds': 48,
+    'unit-no-unknown': true,
+    'value-keyword-case': 'lower',
+    'value-no-vendor-prefix': [
+      true,
+      {
+        ignoreValues: ['box', 'inline-box'],
+      },
+    ],
+  },
+};
